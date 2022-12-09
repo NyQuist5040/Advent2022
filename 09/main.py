39204e9c36268a3d656f09dcc61a4f8f5aa71096
Quentin Jutard <qjutard@acri-st.fr>
2022-12-09 11:19:43 +0100

    question 2, gpt was still broken

--- 09/main.py
+++ 09/main.py
@@ -1,10 +1,11 @@
 import numpy as np
+import copy
 
 # open the file and read the input lines
 with open('09/input.txt') as f:
   instructions = f.read().split('\n')
 
-def simulate_rope_movement_step(head_pos, tail_pos, direction):
+def simulate_rope_movement_step(current_pos, direction):
   # Define a dictionary that associates a 2-tuple with each direction
   directions = {
     'U': np.array((0, 1), np.int32),
@@ -13,29 +14,28 @@ def simulate_rope_movement_step(head_pos, tail_pos, direction):
     'R': np.array((1, 0), np.int32),
   }
 
-  new_head_pos, new_tail_pos = head_pos.copy(), tail_pos.copy()
+  new_pos = copy.deepcopy(current_pos)
 
   # Update the position of the head
-  new_head_pos += directions[direction]
+  new_pos[0] += directions[direction]
 
-  # Update the position of the tail
-  is_adjacent = (abs(new_head_pos[0] - tail_pos[0]) <= 1) and (abs(new_head_pos[1] - tail_pos[1]) <= 1)
-  if not is_adjacent:
-      new_tail_pos[0] += np.sign(new_head_pos[0] - tail_pos[0])
-      new_tail_pos[1] += np.sign(new_head_pos[1] - tail_pos[1])
+  for i in range(1, len(current_pos)):
+    # Update the position of successive knots
+    is_adjacent = (abs(new_pos[i-1][0] - current_pos[i][0]) <= 1) and (abs(new_pos[i-1][1] - current_pos[i][1]) <= 1)
+    if not is_adjacent:
+        new_pos[i][0] += np.sign(new_pos[i-1][0] - current_pos[i][0])
+        new_pos[i][1] += np.sign(new_pos[i-1][1] - current_pos[i][1])
 
   # Return
-  return new_head_pos, new_tail_pos
+  return new_pos
 
-def simulate_rope_movement(instructions):
+def simulate_rope_movement(instructions, n_knots):
 
-  # Initialize the positions of the head and tail
-  head_pos = np.array((0, 0), np.int32)
-  tail_pos = np.array((0, 0), np.int32)
+  # Initialize the positions of all knots, head to tail
+  knot_init_pos = [np.array((0, 0), np.int32) for _ in range(n_knots)]
 
   # Initialize lists to keep track of the positions of the head and tail
-  head_positions = [head_pos]
-  tail_positions = [tail_pos]
+  past_positions = [knot_init_pos]
 
   # Loop through each instruction
   for instruction in instructions:
@@ -44,14 +44,16 @@ def simulate_rope_movement(instructions):
     steps = int(instruction[1:])
 
     for _ in range(steps):
-        new_head_pos, new_tail_pos = simulate_rope_movement_step(head_positions[-1], tail_positions[-1], direction)
-        head_positions.append(new_head_pos)
-        tail_positions.append(new_tail_pos)
+        new_positions = simulate_rope_movement_step(past_positions[-1], direction)
+        past_positions.append(new_positions)
 
   # Return the final position of the tail
-  return head_positions, tail_positions
+  return past_positions
 
-head_positions, tail_positions = simulate_rope_movement(instructions)
+# Question 1: count the number of unique positions of the tail with 2 knots
+rope_positions = simulate_rope_movement(instructions, 2)
+print(len(set(tuple(a[-1]) for a in rope_positions)))
 
-# Question 1: count the number of unique positions of the tail
-print(len(set(tuple(a) for a in tail_positions)))
+# Question 2: count the number of unique positions of the tail with 10 knots
+rope_positions = simulate_rope_movement(instructions, 10)
+print(len(set(tuple(a[-1]) for a in rope_positions)))
