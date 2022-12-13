d543462973e0d5fcebbcacf4cbdc4b08f8d29962
Quentin Jutard <qjutard@acri-st.fr>
2022-12-13 12:42:25 +0100

    day 13 Q2

--- 13/main.py
+++ 13/main.py
@@ -1,16 +1,22 @@
+from functools import cmp_to_key
+import copy
+
 # Read input file
-from struct import pack
-
-from sympy import comp
-
-
 with open('13/input.txt') as f:
     pairs_txt = f.read().split('\n\n')
 
 packet_pairs = []
+all_packets = []
 for pair in pairs_txt:
     pair_left, pair_right = pair.split('\n')
-    packet_pairs.append([eval(pair_left), eval(pair_right)])
+
+    pair_left, pair_right = eval(pair_left), eval(pair_right)
+
+    packet_pairs.append([pair_left, pair_right])
+
+    all_packets.append(pair_left)
+    all_packets.append(pair_right)
+
 
 def compare_packets(packet_left: list, packet_right: list) -> str:
     # Returns 'right' if packet_right is "higher" than packet_left, 'left' for
@@ -53,14 +59,37 @@ def compare_packets(packet_left: list, packet_right: list) -> str:
     else:
         return larger_element
 
+
+def compare_packets_wrapper(packet_left: list, packet_right: list) -> int:
+    result_str = compare_packets(copy.deepcopy(packet_left), copy.deepcopy(packet_right.copy()))
+
+    if result_str == 'right':
+        return 1
+    elif result_str == 'left':
+        return -1
+    else:
+        return 0
+
+
 pair_order = []
 total_Q1 = 0
 for i, pair in enumerate(packet_pairs):
-    order = compare_packets(pair[0], pair[1])
+    order = compare_packets_wrapper(pair[0], pair[1])
     pair_order.append(order)
 
-    if order == 'right':
+    if order == 1:
         total_Q1 += i + 1
 
 # Question 1
 print(total_Q1)
+
+# Question 2
+divider_1 = [[2]]
+divider_2 = [[6]]
+all_packets.append(divider_1)
+all_packets.append(divider_2)
+
+all_packets.sort(key=cmp_to_key(compare_packets_wrapper), reverse=True)
+
+# Question 2
+print((all_packets.index(divider_1) + 1) * (all_packets.index(divider_2) + 1))
