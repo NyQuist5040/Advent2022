dd6209cdbe69f8cd26e728c84aa8b780abf7ccc1
Quentin Jutard <qjutard@acri-st.fr>
2022-12-14 13:51:48 +0100

    day 14 Q2

--- 14/main.py
+++ 14/main.py
@@ -14,6 +14,7 @@ for line_txt in blocks_txt:
 
     blocks.append(new_block)
 
+
 class Cave:
     def __init__(self, blocks) -> None:
 
@@ -39,7 +40,7 @@ class Cave:
         self.v_len = max_v
 
         self.h_offset = min_h
-        self.entry_point = [0, 500 - self.h_offset]
+        self.entry_point = [0, 500]
 
         self.rocks = np.zeros((self.v_len, self.h_len), np.int8)
         self.sand = np.zeros((self.v_len, self.h_len), np.int8)
@@ -60,6 +61,7 @@ class Cave:
                     start_j, end_j = min(corner_1[0], corner_2[0]) - self.h_offset, max(corner_1[0], corner_2[0]) - self.h_offset + 1
                     self.rocks[corner_1[1], start_j:end_j] = 1
 
+
     def __str__(self) -> str:
         full_str = ''
         for i in range(self.v_len):
@@ -74,13 +76,18 @@ class Cave:
 
         return full_str
 
+
     def add_sand(self) -> bool:
         # Adds a grain of sand to the cav, returns True if it was successfully added, False if it
-        # fell from the end.
+        # fell from the end or if the entry point is blocked.
 
-        new_sand_pos = self.entry_point.copy()
+        new_sand_pos = [self.entry_point[0], self.entry_point[1] - self.h_offset]
 
         blocked = self.rocks + self.sand
+
+        # Check if the entry point is blocked
+        if blocked[new_sand_pos[0], new_sand_pos[1]]:
+            return False
 
         while True:
             # Check wether the new sand reached the bottom
@@ -115,7 +122,42 @@ class Cave:
                 break
 
 
+    def empty(self) -> None:
+        self.sand[:, :] = 0
+
+
+    def add_floor(self) -> None:
+        # Adds the floor for question 2, also makes the cave larger to accomodate the maximum
+        # possible sand pile width
+
+        # Initialize the new arrays with adapted size
+        new_v_len = self.v_len + 2
+        new_h_len = (new_v_len * 2) + 1
+        new_rocks = np.zeros((new_v_len, new_h_len), np.int8)
+        new_sand = np.zeros_like(new_rocks)
+
+        # Align the old arrays to the new ones
+        start_j = (1 + new_h_len // 2) - (self.entry_point[1] - self.h_offset)
+        new_rocks[:-2, start_j:(start_j+self.h_len)] = self.rocks
+        new_sand[:-2, start_j:(start_j+self.h_len)] = self.sand
+
+        # Add the floor
+        new_rocks[-1, :] = 1
+
+        # Update parameters
+        self.h_offset = self.h_offset - start_j
+        self.h_len = new_h_len
+        self.v_len = new_v_len
+        self.rocks = new_rocks
+        self.sand = new_sand
+
 # Question 1
 cave = Cave(blocks)
+cave.fill_sand()
+print(np.sum(cave.sand))
+
+# Question 2
+cave.empty()
+cave.add_floor()
 cave.fill_sand()
 print(np.sum(cave.sand))
